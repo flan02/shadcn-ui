c6ac81e0e2210ddf9ec9034b805c5fc0be6ed1b2
flan02 <chanivetdan@hotmail.com>
2024-04-24 19:26:58 -0300

    Nextjs 14.1.0 and shadcn-ui components installed correctly.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Badge from "@/components/training-shadcn/MyBadge";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl text-green-950 text-center">SHADCN-UI THE BASICS</h1>
+      <Badge />
+    </main>
+  );
+}
---
+++ src/components/training-shadcn/MyBadge.tsx
@@ -0,0 +1,13 @@
+import { Badge } from "@/components/ui/badge"
+
+
+type Props = {}
+
+const MyBadge = (props: Props) => {
+  return (
+    <Badge variant="outline" className="text-red-500 bg-red-200">Badge</Badge>
+
+  )
+}
+
+export default MyBadge
